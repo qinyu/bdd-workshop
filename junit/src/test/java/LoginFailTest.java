5e7c754d6d1c28943086f3cb302100d8b4e72f4c
qinyu <qinyusuain@gmail.com>
2017-03-13 08:28:42 +0800

    refactor to parameterized tests

--- junit/src/test/java/LoginFailTest.java
+++ junit/src/test/java/LoginFailTest.java
@@ -4,6 +4,12 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
@@ -11,7 +17,24 @@ import static com.codeborne.selenide.Selenide.$;
 /**
  * Created by yqin on 3/12/17.
  */
+@RunWith(Parameterized.class)
 public class LoginFailTest {
+
+  @Parameters
+  public static Collection<Object[]> data() {
+   return Arrays.asList(new Object[][]{
+       {"admin", "1234567", "ERROR: The password you entered for the username admin is incorrect. Lost your password?"},
+       {"admin1", "123456", "ERROR: Invalid username. Lost your password?"},
+       {"", "123456", "ERROR: The username field is empty."},
+       {"admin", "", "ERROR: The password field is empty."},
+   });
+  }
+
+  public LoginFailTest(String user, String pass, String errorMessage) {
+    this.user = user;
+    this.pass = pass;
+    this.errorMessage = errorMessage;
+  }
 
   @BeforeClass
   public static void setUpClass(){
@@ -23,15 +46,14 @@ public class LoginFailTest {
   @Rule
   public BrowserStrategy browserStrategy = new BrowserStrategy();
 
-  static void login_with_wrong_credentials_and_show_error(String user, String pass, String errorMessage) {
-    LoginTest.login_with_credentials(user, pass);
-    $("#login_error")
-        .shouldHave(text(errorMessage));
-  }
+
+  private String user, pass, errorMessage;
 
   @Test
-  public void should_show_error_message_if_login_with_wrong_pass() {
-    login_with_wrong_credentials_and_show_error("admin", "1234567", "ERROR: The password you entered for the username admin is incorrect. Lost your password?");
+  public void should_show_error_message_if_login_with_wrong_credentials() {
+    LoginTest.login_with_credentials(this.user, this.pass);
+    $("#login_error")
+        .shouldHave(text(this.errorMessage));
   }
 
   @Before
@@ -39,18 +61,5 @@ public class LoginFailTest {
     LoginTest.gotoLoginPage();
   }
 
-  @Test
-  public void should_show_error_message_if_login_with_wrong_user() {
-    login_with_wrong_credentials_and_show_error("admin1", "123456", "ERROR: Invalid username. Lost your password?");
-  }
 
-  @Test
-  public void should_show_error_message_if_login_with_empty_user() {
-    login_with_wrong_credentials_and_show_error("", "123456", "ERROR: The username field is empty.");
-  }
-
-  @Test
-  public void should_show_error_message_if_login_with_empty_pass() {
-    login_with_wrong_credentials_and_show_error("admin", "", "ERROR: The password field is empty.");
-  }
 }
